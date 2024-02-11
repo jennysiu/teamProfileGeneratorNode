2222a34360f17b72fc98bcea801ecebe3250bb7f
jennysiu <jenny.siu79@gmail.com>
2024-02-11 20:17:35 +0000

    manager card on its own level

--- src/page-template.js
+++ src/page-template.js
@@ -4,18 +4,22 @@ const generateTeam = team => {
     // creates the manager html
     const generateManager = manager => {
         return `
-                    <div class="col-12 col-md-6 col-lg-4">
-                        <div class="card employee-card manager">
-                            <div class="card-header">
-                                <h2 class="card-title">${manager.getName()}</h2>
-                                <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
-                            </div>
-                            <div class="card-body">
-                                <ul class="list-group">
-                                    <li class="list-group-item">ID: ${manager.getId()}</li>
-                                    <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
-                                    <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
-                                </ul>
+                    <div class=container manager-container>
+                        <div class=row>
+                            <div class="col-12 col-md-6 col-lg-4">
+                                <div class="card employee-card manager">
+                                    <div class="card-header">
+                                        <h2 class="card-title">${manager.getName()}</h2>
+                                        <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
+                                    </div>
+                                    <div class="card-body">
+                                        <ul class="list-group">
+                                            <li class="list-group-item">ID: ${manager.getId()}</li>
+                                            <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+                                            <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
+                                        </ul>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
